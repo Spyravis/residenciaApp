912d586e4dc0b505945e4ec1900a5c7133a90e8c
MATIAS EXEQUIEL CECCI <matias_cecci@hotmail.com>
2023-02-05 11:36:19 +0000

    maquetado navbar, rutas layout, creado paginas login,contactUs y aboutUs

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light p-3">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">ACA VA EL LOGO</span>
+        </Link>
+        <div
+          class="btn-toolbar"
+          role="toolbar"
+          aria-label="Toolbar with button groups"
+        >
+          <div className="btn-group me-2" role="group" aria-label="First group">
+            <Link to="/aboutUs">
+              <button className="btn btn-warning">About Us</button>
+            </Link>
+          </div>
+          <div
+            className="btn-group me-2"
+            role="group"
+            aria-label="Second group"
+          >
+            <Link to="/contactUs">
+              <button type="button" className="btn btn-success">
+                Contact Us
+              </button>
+            </Link>
+          </div>
+          <div className="btn-group" role="group" aria-label="Third group">
+            <Link to="/login">
+              <button type="button" className="btn btn-primary">
+                Área Personal
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
