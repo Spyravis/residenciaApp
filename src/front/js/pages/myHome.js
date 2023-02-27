dfec40b4dcfea6f0b98eba4e986b2c9d2bdf307b
MATIAS EXEQUIEL CECCI <matias_cecci@hotmail.com>
2023-02-27 18:24:24 +0000

    merge componente_perfil

--- src/front/js/pages/myHome.js
+++ src/front/js/pages/myHome.js
@@ -1,13 +1,13 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 import { LoggedMenu } from "../component/logged-menu";
 import "../../styles/home.css";
 
 export const MyHome = () => {
-    const navigate = useNavigate();
-    const { store, actions } = useContext(Context);
-    /*
+  const navigate = useNavigate();
+  const { store, actions } = useContext(Context);
+  /*
         if (store.userdata == null) {
             console.log("VACIOOO");
             localStorage.getItem("user");
@@ -18,22 +18,20 @@ export const MyHome = () => {
         }
     */
 
-    return (
-
-        <div className="container text-center mt-5">
-            <div className="row">
-                <h2>{store.userdata.role_user == 1 ? "Familiar" : "Trabajador"} {store.userdata.name}</h2>
-            </div>
-            <LoggedMenu></LoggedMenu>
-            <div className="row">
-
-                {Object.keys(store.userdata).map((x, index) => {
-                    return (
-                        <p key={index}>{x + " " + store.userdata[x]}</p>
-                    );
-                })}
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="container text-center mt-5">
+      <div className="row">
+        <h2>
+          {store.userdata.role_user == 1 ? "Familiar" : "Trabajador"}{" "}
+          {store.userdata.name}
+        </h2>
+      </div>
+      <LoggedMenu></LoggedMenu>
+      <div className="row">
+        {Object.keys(store.userdata).map((x, index) => {
+          return <p key={index}>{x + " " + store.userdata[x]}</p>;
+        })}
+      </div>
+    </div>
+  );
 };
