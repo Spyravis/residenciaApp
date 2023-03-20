78e9d0a8916cfb57e40bf24af81eeca6cc73ffaf
Spyravis <ritalopezcobo@gmail.com>
2023-03-20 16:59:05 +0000

    fin

--- src/front/js/pages/partes.js
+++ src/front/js/pages/partes.js
@@ -4,12 +4,14 @@ import { LoggedMenu } from "../component/logged-menu";
 import { ParteNocturno } from "../component/parteNocturno";
 import { ParteQuincenal } from "../component/parteQuincenal";
 import { useParams } from "react-router-dom";
+import "../../styles/partes.css";
 
 export const Partes = () => {
   const [showNoc, setShowNoc] = useState(true);
   const [showQuin, setShowQuin] = useState(false);
   const navigate = useNavigate();
   const params = useParams();
+  //const { store, actions } = useContext(Context);
   const displayNoc = () => {
     setShowNoc(true);
     setShowQuin(false);
@@ -18,26 +20,24 @@ export const Partes = () => {
     setShowNoc(false);
     setShowQuin(true);
   };
+
+  /*useEffect(() => {
+    if (!store.userdata.id) {
+      navigate("/login");
+    }
+  }, []);*/
   return (
-    <div className="container">
+    <div className="partes">
       <LoggedMenu />
       <div className="d-flex justify-content-center p-5">
-        <button className="btn btn-primary" onClick={displayNoc}>
+        <button className="btn btn-1" onClick={displayNoc}>
           Parte Nocturno
         </button>
-        <button className="btn btn-secondary" onClick={displayQuin}>
+        <button className="btn btn-2" onClick={displayQuin}>
           Parte Quincenal
         </button>
       </div>
       {showNoc ? <ParteNocturno /> : showQuin ? <ParteQuincenal /> : null}
-      <div className="d-flex justify-content-center p-5">
-        <button
-          className="btn btn-primary"
-          onClick={() => navigate("/historial")}
-        >
-          Historial
-        </button>
-      </div>
     </div>
   );
 };
